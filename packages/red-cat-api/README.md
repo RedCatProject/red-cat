ad8d0d6c013c615509dab507c40fdc3eb749a134
Vladimir.Ovsyanikov <vladovpro@gmail.com>
2019-10-04 17:34:29 +0300

    Remove react

--- packages/red-cat-api/README.md
+++ packages/red-cat-api/README.md
@@ -76,18 +76,6 @@ ELASTIC_APM_SERVICE_NAME=
 
 https://33ced70aaa8741369e2b97fa6f04f08f.eu-central-1.aws.cloud.es.io:9243/login?next=%2F
 
-
-
-## Support
-
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
-
-## Stay in touch
-
-- Author - [Kamil Myśliwiec](https://kamilmysliwiec.com)
-- Website - [https://nestjs.com](https://nestjs.com/)
-- Twitter - [@nestframework](https://twitter.com/nestframework)
-
 ## License
 
   Nest is [MIT licensed](LICENSE).
--- packages/red-cat-api/src/app.module.ts
+++ packages/red-cat-api/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApmModule } from 'nestjs-apm';
+import { CandidatesModule } from './candidates/candidates.module';
+import { EventsModule } from './events/events.module';
+import { VacanciesModule } from './vacancies/vacancies.module';
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
 
 // logging service
-import { ApmModule } from 'nestjs-apm';
 
 @Module({
   imports: [
     // ApmModule.register(),
-  ],
+    CandidatesModule,
+    EventsModule,
+    VacanciesModule,
+    UsersModule,
+    CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })
